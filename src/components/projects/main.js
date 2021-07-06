40c8c07d598a9add058951166773be039c7a03b0
BALU KRISHNA R <balukrs@outlook.com>
2021-07-06 20:52:58 +0530

    parallax implemented

--- src/components/projects/main.js
+++ src/components/projects/main.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import build from "../../assets/parallax/building.png";
-import dune from "../../assets/parallax/dune.png";
+import React, { useRef, useState, useLayoutEffect } from "react";
+import mountain from "../../assets/parallax/mountains.png";
+import woods from "../../assets/parallax/woods.png";
 
 import {
   useViewportScroll,
@@ -11,31 +11,71 @@ import {
 
 const Projectmain = () => {
   const { scrollY } = useViewportScroll();
+  const [offsetTopwood, setOffsetTopwood] = useState(0);
+  const woodref = useRef();
+  const bgref = useRef();
+  const y1 = useTransform(
+    scrollY,
+    [offsetTopwood + 400, offsetTopwood - 400],
+    [0, 500]
+  );
+  const y2 = useTransform(
+    scrollY,
+    [offsetTopwood - 400, offsetTopwood + 400],
+    [0, 20]
+  );
+  const y3 = useTransform(
+    scrollY,
+    [offsetTopwood - 500, offsetTopwood + 500],
+    [0, 350]
+  );
 
-  console.log(scrollY);
-
-  const y1 = useTransform(scrollY, [0, 300], [0, 300]);
-  const y2 = useTransform(scrollY, [0, 300], [0, -100]);
+  useLayoutEffect(() => {
+    if (!woodref.current) return null;
+    setOffsetTopwood(woodref.current.offsetTop);
+  }, [woodref]);
 
   return (
     <motion.div
       className="projectht"
-      style={{
-        backgroundImage:
-          "linear-gradient(to bottom, #ffffff, #efefef, #dfdfdf, #cfcfcf, #bfbfbf)",
-      }}
+      // style={{
+      //   backgroundImage:
+      //     "linear-gradient(to bottom, #ffffff, #efefef, #dfdfdf, #cfcfcf, #bfbfbf)",
+      // }}
     >
-      <section className="h-screen ">
-        <div className="relative">
-          <motion.div className="m-auto" style={{ y: y1 }}>
-            <h1>PROJECTS</h1>
-          </motion.div>
-          <motion.div className="m-auto" style={{ y: y2 }}>
-            <h1>C'MON</h1>
-          </motion.div>
-        </div>
+      <section
+        className="relative h-screen"
+        style={{
+          minHeight: "auto",
+        }}
+      >
+        <motion.h2
+          className="absolute text-6xl font-ibm font text-gray-50 top-1/3 left-2/4"
+          style={{
+            transform: "translate(-50%,-33.33%)",
+            y: y3,
+            x: "-100px",
+            zIndex: "-1",
+          }}
+        >
+          Projects.
+        </motion.h2>
+        <motion.img
+          ref={bgref}
+          src={mountain}
+          className="absolute object-cover w-full min-h-full"
+          style={{ zIndex: "-2", y: y2 }}
+        />
+        <motion.img
+          ref={woodref}
+          src={woods}
+          className="absolute object-cover w-full min-h-full"
+          style={{ zIndex: "-2", y: y1 }}
+        />
       </section>
-      <section className="h-screen ">H</section>
+      <section className="h-screen bg-gray-700" style={{ zIndex: "2" }}>
+        H
+      </section>
       <section className="h-screen ">T</section>
     </motion.div>
   );
