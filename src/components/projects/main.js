1a5fbc3f59eb2bb8ddd6b29ecae45b06b83ea46c
BALU KRISHNA R <balukrs@outlook.com>
2021-07-07 10:43:09 +0530

    parallax corrected

--- src/components/projects/main.js
+++ src/components/projects/main.js
@@ -1,13 +1,20 @@
 import React, { useRef, useState, useLayoutEffect } from "react";
-import mountain from "../../assets/parallax/mountains.png";
-import woods from "../../assets/parallax/woods.png";
+import mountain from "../../assets/parallax/mountains_new.png";
+import woods from "../../assets/parallax/woods_new.png";
 
-import {
-  useViewportScroll,
-  useTransform,
-  useSpring,
-  motion,
-} from "framer-motion";
+import { useViewportScroll, useTransform, motion } from "framer-motion";
+
+const mainVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+    },
+  },
+};
 
 const Projectmain = () => {
   const { scrollY } = useViewportScroll();
@@ -38,24 +45,30 @@ const Projectmain = () => {
   return (
     <motion.div
       className="projectht"
+      variants={mainVariants}
+      initial="hidden"
+      animate="visible"
       // style={{
       //   backgroundImage:
       //     "linear-gradient(to bottom, #ffffff, #efefef, #dfdfdf, #cfcfcf, #bfbfbf)",
       // }}
     >
       <section
-        className="relative h-screen"
+        className="relative h-screen img-wrap"
         style={{
           minHeight: "auto",
         }}
       >
         <motion.h2
-          className="absolute text-6xl font-ibm font text-gray-50 top-1/3 left-2/4"
+          className="absolute font-extrabold text-7xl font-ibm font text-gray-50 top-1/3 left-2/4"
           style={{
             transform: "translate(-50%,-33.33%)",
             y: y3,
-            x: "-100px",
+            x: "-150px",
             zIndex: "-1",
+            color: "rgb(220, 222, 224)",
+            textShadow:
+              " 2px 8px 6px rgba(0,0,0,0.2),0px -5px 35px rgba(255,255,255,0.3)",
           }}
         >
           Projects.
@@ -63,9 +76,10 @@ const Projectmain = () => {
         <motion.img
           ref={bgref}
           src={mountain}
-          className="absolute object-cover w-full min-h-full"
+          className="absolute object-cover w-full min-h-full white bg-g"
           style={{ zIndex: "-2", y: y2 }}
         />
+
         <motion.img
           ref={woodref}
           src={woods}
@@ -73,7 +87,12 @@ const Projectmain = () => {
           style={{ zIndex: "-2", y: y1 }}
         />
       </section>
-      <section className="h-screen bg-gray-700" style={{ zIndex: "2" }}>
+      <section
+        className="h-screen mt-8 bg-gradient-to-t from-gray-100 via-gray-300 to-white"
+        style={{
+          zIndex: "2",
+        }}
+      >
         H
       </section>
       <section className="h-screen ">T</section>
