5efc021cab09e3fc301dee68a1b5bea5b0e857c0
Balu Krishna <balukrs@outlook.com>
2021-07-10 08:49:05 +0530

    new direction

--- src/components/projects/main.js
+++ src/components/projects/main.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Projpic from "../../assets/parallax/project.webp";
 import { motion } from "framer-motion";
 
 const mainVariants = {
@@ -21,7 +21,27 @@ const Projectmain = () => {
       variants={mainVariants}
       initial="hidden"
       animate="visible"
-    ></motion.div>
+    >
+      <section className="w-full h-screen">
+        <div>
+          <span>P</span>
+          <span>R</span>
+          <span>O</span>
+          <span>J</span>
+          <span>E</span>
+          <span>C</span>
+          <span>T</span>
+          <span>S</span>
+          <span>.</span>
+        </div>
+        <div
+          className="flex items-center justify-center inline-block w-2/4 m-auto bg-cover rounded-xl h-3/4"
+          style={{
+            backgroundImage: `url(${Projpic})`,
+          }}
+        ></div>
+      </section>
+    </motion.div>
   );
 };
 
