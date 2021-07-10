9ddf24b5ff8b845e640bd1f01670a07a86a963d0
Balu Krishna <balukrs@outlook.com>
2021-07-10 12:18:46 +0530

    Check

--- src/components/projects/main.js
+++ src/components/projects/main.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Projpic from "../../assets/parallax/project.webp";
+import babypic from "../../assets/parallax/baby.png";
+import ballpic from "../../assets/parallax/ball.png";
+import earthpic from "../../assets/parallax/earth.png";
+import plantpic from "../../assets/parallax/plant.png";
+
 import { motion } from "framer-motion";
 
 const mainVariants = {
@@ -22,8 +27,8 @@ const Projectmain = () => {
       initial="hidden"
       animate="visible"
     >
-      <section className="w-full h-screen">
-        <div>
+      <section className="relative w-full h-screen">
+        <div className="absolute">
           <span>P</span>
           <span>R</span>
           <span>O</span>
@@ -35,11 +40,20 @@ const Projectmain = () => {
           <span>.</span>
         </div>
         <div
-          className="flex items-center justify-center inline-block w-2/4 m-auto bg-cover rounded-xl h-3/4"
+          className="flex inline-block w-2/4 m-auto bg-cover border-8 border-gray-200 rounded-sm h-3/4"
           style={{
             backgroundImage: `url(${Projpic})`,
           }}
         ></div>
+        <div className="absolute top-0 left-0 w-1/5 opacity-50 h-1/5">
+          <img src={ballpic} />
+        </div>
+        <div className="absolute right-0 w-1/5 top-48 opacity-80 h-1/5">
+          <img src={earthpic} />
+        </div>
+        <div className="absolute left-0 w-1/5 bottom-20 h-1/5 opacity-80">
+          <img src={plantpic} />
+        </div>
       </section>
     </motion.div>
   );
