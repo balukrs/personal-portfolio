1615b4700232450f8e9525b8eeb0ceb6beb7e6ea
BALU KRISHNA R <balukrs@outlook.com>
2021-07-15 21:02:57 +0530

    projects getting completed

--- src/components/projects/grid.js
+++ src/components/projects/grid.js
@@ -1,17 +1,19 @@
-import React, { useLayoutEffect, useRef } from "react";
-import testimg from "../../assets/test.webp";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { AiFillGithub } from "react-icons/ai";
 import { RiPagesLine } from "react-icons/ri";
 
 import todo_face from "../../assets/projects/todo_app/front.webp";
-import todo_cover from "../../assets/projects/todo_app/cover.webp";
 import streamer_face from "../../assets/projects/streamer_app/front.webp";
-import streamer_cover from "../../assets/projects/streamer_app/cover.webp";
 import social_face from "../../assets/projects/social_app/front.webp";
-import social_cover from "../../assets/projects/social_app/cover.webp";
 import chat_face from "../../assets/projects/chat_app/front.webp";
-import chat_cover from "../../assets/projects/chat_app/cover.webp";
+import exercise_face from "../../assets/projects/exercise_app/front.webp";
+import portfolio_face from "../../assets/projects/portfolio_app/front.webp";
+
+import firebase_logo from "../../assets/logos/firebase.png";
+import mongo_logo from "../../assets/logos/mongo.png";
+import sass_logo from "../../assets/logos/sass.png";
+import node_logo from "../../assets/logos/node.png";
 
 const Grid = ({ layoutpos }) => {
   const mainheadref = useRef();
@@ -47,15 +49,15 @@ const Grid = ({ layoutpos }) => {
     return (
       <>
         <div
-          className="absolute bottom-0 left-0 right-0 flex items-center justify-around h-12 bg-gray-300 bg-opacity-30"
+          className="absolute bottom-0 left-0 right-0 flex items-center justify-around w-full h-12 bg-gray-400 bg-opacity-30"
           style={{
-            borderRadius: "0 0 20px 20px",
+            borderRadius: "0 0 5px 5px",
           }}
         >
-          <a className="cursor-pointer hover:text-red-400">
+          <a className="cursor-pointer hover:text-red-500">
             <AiFillGithub size="2em" />
           </a>
-          <a className="cursor-pointer hover:text-red-400">
+          <a className="cursor-pointer hover:text-red-500">
             <RiPagesLine size="2em" />
           </a>
         </div>
@@ -69,7 +71,7 @@ const Grid = ({ layoutpos }) => {
   };
 
   useLayoutEffect(() => {
-    if (layoutpos > mainheadref.current.offsetTop - 400) {
+    if (layoutpos > mainheadref.current.offsetTop - 450) {
       animeOrch1();
     }
   }, [layoutpos]);
@@ -97,12 +99,16 @@ const Grid = ({ layoutpos }) => {
             <img
               src={todo_face}
               style={{
-                borderRadius: "20px 20px 0 0",
+                borderRadius: "5px 5px 0 0",
               }}
             />
           </div>
           <div className="relative details">
-            <h3>Heading 2</h3>
+            <h3 className="text-xl font-bold">To Do App</h3>
+            <div className="relative flex mb-1">
+              <img src={firebase_logo} className="object-cover w-5/12" />
+              <img src={sass_logo} className="object-cover w-5/12" />
+            </div>
             <p className="mb-10">
               arGraph is a generic parallel graph library. It is comparable with
               Boost Graph Library (BGL). Like the BGL it uses C++ templates and
@@ -116,12 +122,16 @@ const Grid = ({ layoutpos }) => {
             <img
               src={streamer_face}
               style={{
-                borderRadius: "20px 20px 0 0",
+                borderRadius: "5px 5px 0 0",
               }}
             />
           </div>
           <div className="relative details">
-            <h3>Heading 2</h3>
+            <h3 className="text-xl font-bold">Streamer App</h3>
+            <div className="relative flex mb-1">
+              <img src={node_logo} className="object-cover w-5/12" />
+              <img src={mongo_logo} className="object-cover w-5/12" />
+            </div>
             <p className="mb-10">
               arGraph is a generic parallel graph library. It is comparable with
               Boost Graph Library (BGL). Like the BGL it uses C++ templates and
@@ -135,7 +145,7 @@ const Grid = ({ layoutpos }) => {
             <img
               src={social_face}
               style={{
-                borderRadius: "20px 20px 0 0",
+                borderRadius: "5px 5px 0 0",
               }}
             />
           </div>
@@ -154,12 +164,50 @@ const Grid = ({ layoutpos }) => {
             <img
               src={chat_face}
               style={{
-                borderRadius: "20px 20px 0 0",
+                borderRadius: "5px 5px 0 0",
               }}
             />
           </div>
           <div className="relative details">
             <h3>Heading 4</h3>
+            <p className="mb-10">
+              arGraph is a generic parallel graph library. It is comparable with
+              Boost Graph Library (BGL). Like the BGL it uses C++ templates and
+              the visitor concept to generically implement graph algorithms.
+            </p>
+            <Btnnav />
+          </div>
+        </div>
+        <div className="card">
+          <div className="img_container">
+            <img
+              src={exercise_face}
+              style={{
+                borderRadius: "5px 5px 0 0",
+              }}
+            />
+          </div>
+          <div className="relative details">
+            <h3>Heading 5</h3>
+            <p className="mb-10">
+              arGraph is a generic parallel graph library. It is comparable with
+              Boost Graph Library (BGL). Like the BGL it uses C++ templates and
+              the visitor concept to generically implement graph algorithms.
+            </p>
+            <Btnnav />
+          </div>
+        </div>
+        <div className="card">
+          <div className="img_container">
+            <img
+              src={portfolio_face}
+              style={{
+                borderRadius: "5px 5px 0 0",
+              }}
+            />
+          </div>
+          <div className="relative details">
+            <h3>Heading 6</h3>
             <p className="mb-10">
               arGraph is a generic parallel graph library. It is comparable with
               Boost Graph Library (BGL). Like the BGL it uses C++ templates and
