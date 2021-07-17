74950b53b209a172876bc4765b7bcc033d26194e
BALU KRISHNA R <balukrs@outlook.com>
2021-07-17 13:41:43 +0530

    React projects completed

--- src/components/projects/grid.js
+++ src/components/projects/grid.js
@@ -46,19 +46,41 @@ const Grid = ({ layoutpos }) => {
       },
     },
   };
+  const ReadMore = ({ children }) => {
+    const text = children;
+    const [isReadMore, setIsReadMore] = useState(true);
+    const toggleReadMore = () => {
+      setIsReadMore(!isReadMore);
+    };
+    return (
+      <p>
+        {isReadMore ? text.slice(0, 210) : text}
+        <span onClick={toggleReadMore} className="text-gray-300 cursor-pointer">
+          {isReadMore ? "...read more" : " show less"}
+        </span>
+      </p>
+    );
+  };
 
-  const Btnnav = () => {
+  const Btnnav = ({ stats }) => {
     return (
       <>
         <div
-          className="absolute bottom-0 left-0 right-0 flex items-center justify-around w-full h-12 bg-gray-400 bg-opacity-30"
+          className="flex items-center justify-around w-full h-12 bg-gray-400 bg-opacity-30"
           style={{
             borderRadius: "0 0 5px 5px",
           }}
         >
-          <a className="cursor-pointer hover:text-red-500">
+          <a className="flex items-center justify-center cursor-pointer hover:text-red-500">
             <AiFillGithub size="2em" />
+            <p className="text-sm ">Code</p>
           </a>
+          {stats == true ? (
+            <a className="flex items-center justify-center cursor-pointer hover:text-red-500">
+              <AiFillGithub size="2em" />
+              <p className="text-sm ">Server</p>
+            </a>
+          ) : null}
           <a className="cursor-pointer hover:text-red-500">
             <RiPagesLine size="2em" />
           </a>
@@ -73,7 +95,7 @@ const Grid = ({ layoutpos }) => {
   };
 
   useLayoutEffect(() => {
-    if (layoutpos > mainheadref.current.offsetTop - 450) {
+    if (layoutpos > mainheadref.current.offsetTop - 550) {
       animeOrch1();
     }
   }, [layoutpos]);
@@ -106,20 +128,18 @@ const Grid = ({ layoutpos }) => {
             />
           </div>
           <div className=" details">
-            <h3 className="text-xl font-bold">To Do App</h3>
+            <h3 className="text-xl font-bold ">To Do App</h3>
             <div className="relative flex mb-1">
               <img src={firebase_logo} className="object-cover w-5/12" />
               <img src={sass_logo} className="object-cover w-5/12" />
             </div>
-            <p className="mb-10">
+            <ReadMore>
               A simple react JS application to keep track of your activities.
-              <span className="font-bold "> Firebase</span> is implemented for
-              authentication purpose and for storing data making use of its
-              <span className="font-bold "> Realtime Database</span>. The
-              components are styled using{" "}
-              <span className="font-bold "> SASS</span> and Material-UI. Dark
-              mode is also added.
-            </p>
+              Firebase is implemented for authentication purpose and for storing
+              data making use of its Realtime Database.Custom hooks are used to
+              interact with firebase database. The components are styled using
+              SASS and Material-UI. Dark mode is also added.
+            </ReadMore>
           </div>
           <Btnnav />
         </div>
@@ -132,27 +152,22 @@ const Grid = ({ layoutpos }) => {
               }}
             />
           </div>
-          <div
-            className="overflow-hidden  details"
-            style={{
-              maxHeight: "300px",
-            }}
-          >
+          <div className="h-auto details">
             <h3 className="text-xl font-bold">Streamer App</h3>
             <div className="relative flex mb-1">
               <img src={node_logo} className="object-cover w-5/12" />
               <img src={mongo_logo} className="object-cover w-5/12" />
             </div>
-            <p className="mb-10">
+            <ReadMore>
               A simple react JS application that portrays the functions of a
               streaming app where you can edit,delete and create streams. The
               elements are styled using Tailwind CSS and Material-UI. Redux is
               implemented for state management. The server is created using
               express JS and mongoose is used for object mapping between the
               server and mongoDB Atlas database.
-            </p>
+            </ReadMore>
           </div>
-          <Btnnav />
+          <Btnnav stats={true} />
         </div>
         <div className="relative card">
           <div className="img_container">
@@ -169,13 +184,16 @@ const Grid = ({ layoutpos }) => {
               <img src={node_logo} className="object-cover w-5/12" />
               <img src={mongo_logo} className="object-cover w-5/12" />
             </div>
-            <p className="mb-10">
-              arGraph is a generic parallel graph library. It is comparable with
-              Boost Graph Library (BGL). Like the BGL it uses C++ templates and
-              the visitor concept to generically implement graph algorithms.
-            </p>
+            <ReadMore>
+              A simple facebook clone where users can create account to post
+              status and photos. Redux is used for state management and
+              Material-UI designs are used. Authentication is done by JWT stored
+              inside an httpOnly cookie. express JS server is used and mongoDB
+              cloud Atlas database is connected using mongoose. bcrypt is used
+              to hash the passwords stored in database.
+            </ReadMore>
           </div>
-          <Btnnav />
+          <Btnnav stats={true} />
         </div>
         <div className="relative card">
           <div className="img_container">
@@ -192,13 +210,16 @@ const Grid = ({ layoutpos }) => {
               <img src={node_logo} className="object-cover w-5/12" />
               <img src={socket_logo} className="object-cover w-5/12" />
             </div>
-            <p className="mb-10">
-              arGraph is a generic parallel graph library. It is comparable with
-              Boost Graph Library (BGL). Like the BGL it uses C++ templates and
-              the visitor concept to generically implement graph algorithms.
-            </p>
+            <ReadMore>
+              A chat application created with express JS and Socket IO. Users
+              can enter the chat room with an unique username thus enabling
+              realtime, bi-directional communication between web clients and
+              servers. A new browser window needs to be open to establish a
+              connection and add a new user. The user can leave or disconnect
+              from the chat room by closing the browser window.
+            </ReadMore>
           </div>
-          <Btnnav />
+          <Btnnav stats={true} />
         </div>
         <div className="relative card">
           <div className="img_container">
@@ -215,13 +236,15 @@ const Grid = ({ layoutpos }) => {
               <img src={node_logo} className="object-cover w-5/12" />
               <img src={mongo_logo} className="object-cover w-5/12" />
             </div>
-            <p className="mb-10">
-              arGraph is a generic parallel graph library. It is comparable with
-              Boost Graph Library (BGL). Like the BGL it uses C++ templates and
-              the visitor concept to generically implement graph algorithms.
-            </p>
+            <ReadMore>
+              A react app customized using Tailwind CSS for keeping track of
+              daily exercise activities. Backend of the application is created
+              with express JS and mongoDB atlas is used as database for storing
+              user and activity details. Users can create,edit and delete their
+              exercise logs.
+            </ReadMore>
           </div>
-          <Btnnav />
+          <Btnnav stats={true} />
         </div>
         <div className="relative card">
           <div className="img_container">
@@ -237,11 +260,12 @@ const Grid = ({ layoutpos }) => {
             <div className="relative flex mb-1">
               <img src={framer_logo} className="object-cover w-5/12" />
             </div>
-            <p className="mb-10">
-              arGraph is a generic parallel graph library. It is comparable with
-              Boost Graph Library (BGL). Like the BGL it uses C++ templates and
-              the visitor concept to generically implement graph algorithms.
-            </p>
+            <ReadMore>
+              React is used to create my personal portfolio website. Styled with
+              Tailwind CSS and framer motion library is used for creating
+              animations. React Context is used for managing state.The website
+              includes many of my personal projects that i did to learn react.
+            </ReadMore>
           </div>
           <Btnnav />
         </div>
